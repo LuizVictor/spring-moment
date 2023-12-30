aa96b7547ffaafe43179163b9ccdb3d3878ecdf6
Luiz Victor <luizvictor.dev@outlook.com>
2023-12-30 10:18:34 -0300

    first commit

---
+++ src/main/java/br/com/luizvictor/springmoment/SpringMomentApplication.java
@@ -0,0 +1,13 @@
+package br.com.luizvictor.springmoment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMomentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMomentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/luizvictor/springmoment/SpringMomentApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luizvictor.springmoment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMomentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
